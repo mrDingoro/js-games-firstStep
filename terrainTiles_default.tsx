eda59fb7d80b9a95f36ba8708b0dda70571ae104
Mikhail <mrdingoro@gmail.com>
2020-04-13 16:36:35 +0300

    Add files

---
+++ terrainTiles_default.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="terrainTiles_default" tilewidth="32" tileheight="32" tilecount="160" columns="20">
+ <image source="../Tilesheet/terrainTiles_default.png" width="640" height="256"/>
+</tileset>
